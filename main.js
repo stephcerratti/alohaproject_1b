89de7beb8527b59329cecfdb0f1d9032923a64d0
stephcerratti <stephanie.cerratti@gmail.com>
2017-01-23 09:56:50 -0500

    #04: fourth commit
    Owl slider and add to cart button added.  Improvements to tablet and
    desktop versions.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 $('.owl-carousel').owlCarousel({
+    rtl:true,
     loop:true,
     margin:10,
     nav:true,
@@ -9,28 +10,40 @@ $('.owl-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:5
+            items:4
         }
     }
 })
 
-})
-var addToCart =document.getElementById("addToCart");
-addToCart.addEventListener("click", changeCartQuantity,false);
-function changeCartQuantity() {
-            var itemQuantity = 0;
-            // console.log("I am in the function");
-            // is quantitiy zero?       
-                if (itemQuantity == 0) {
-                    // if yes, then add one to quantity
-                    itemQuantity = itemQuantity + 1;
-                } else {
-                    // if no, then add one to exisiting quantity
-                    itemQuantity = itemQuantity + 1;
-                }
-                    // then display new quantity
-                document.getElementById("item-quantity").innerHTML = itemQuantity;
-}
+
+$(".addToCart").on("click", function (){
+    var inputField = parseInt($("#cartQuantity").val()); 
+    // increaSE VALUE BY WHEN CLICK THE BUTTON            
+     $("#cartQuantity").val(inputField + 1);      
+     });      
+     $(".addToCart").click(function(){ 
+     //shows the input for cart quantities     
+     $("#cartQuantity").show(); 
+ })
+ });
+
+
+// var addToCart =document.getElementById("addToCart");
+// addToCart.addEventListener("click", changeCartQuantity,false);
+// function changeCartQuantity() {
+//             var itemQuantity = 0;
+//             // console.log("I am in the function");
+//             // is quantitiy zero?       
+//                 if (itemQuantity == 0) {
+//                     // if yes, then add one to quantity
+//                     itemQuantity = itemQuantity + 1;
+//                 } else {
+//                     // if no, then add one to exisiting quantity
+//                     itemQuantity = itemQuantity + 1;
+//                 }
+//                     // then display new quantity
+//                 document.getElementById("item-quantity").innerHTML = itemQuantity;
+// // }
